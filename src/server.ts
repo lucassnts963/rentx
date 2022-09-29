0c3476df6c348aeae262199cb7cfcb98a8d6138c
Lucas Santos <lucassnts963@gmail.com>
2022-09-29 13:33:12 -0300

    refatorando rotas

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
+import { router } from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', specificationsRoutes);
-
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
-});
-
-app.post('/courses', (request, response) => {
-  const { name } = request.body;
-
-  return response.json({ name });
-});
+app.use(router);
 
 app.listen(3333, () => console.log('Servidor iniciado!'));
