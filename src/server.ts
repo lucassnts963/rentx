a690e705e5c529b9a02a5dfa352b219c4efedfde
Lucas Santos <lucassnts963@gmail.com>
2022-09-29 16:17:52 -0300

    docs: adicionando swagger para gerar documentação

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 
 import { router } from './routes';
+
+import swaggerFile from './swagger.json';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(router);
 
 app.listen(3333, () => console.log('Servidor iniciado!'));
