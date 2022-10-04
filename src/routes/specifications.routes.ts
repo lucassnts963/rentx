b467b8751faac7b3287a8b8a6b9ea7f683da3839
Lucas Santos <lucassnts963@gmail.com>
2022-10-04 11:19:34 -0300

    feat: criando middlewares para autenticação de rotas

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { CreateSpecificationController } from '../modules/cars/useCases/createSpecification/CreateSpecificationController';
 import { ListCategoriesController } from '../modules/cars/useCases/listCategories/ListCategoriesController';
 
@@ -7,8 +8,8 @@ const specificationsRoutes = Router();
 const createSpecificationController = new CreateSpecificationController();
 const listSpecificationsController = new ListCategoriesController();
 
+specificationsRoutes.use(ensureAuthenticated);
 specificationsRoutes.post('/', createSpecificationController.handle);
-
 specificationsRoutes.get('/', listSpecificationsController.handle);
 
 export { specificationsRoutes };
